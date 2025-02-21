bb32efbf24e75fe80f35460f1aa16827d2d2e74b
Kenneth Camacho <camachokennethf@gmail.com>
2025-02-21 21:18:30 +0800

    initial commit

---
+++ resources/js/Layouts/GuestLayout.jsx
@@ -0,0 +1,16 @@
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
+
+export default function GuestLayout({ children }) {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      {/* <div>
+                <Link href="/">
+                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                </Link>
+            </div> */}
+
+      <div className="w-full max-w-sm">{children}</div>
+    </div>
+  );
+}
---
+++ resources/js/Pages/Auth/Login.jsx
@@ -0,0 +1,169 @@
+import Checkbox from "@/Components/Checkbox";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/Components/ui/button";
+import { cn } from "@/lib/utils";
+
+export default function Login({
+  className,
+  status,
+  canResetPassword,
+  ...props
+}) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: "",
+    password: "",
+    remember: false,
+  });
+
+  const submit = (e) => {
+    e.preventDefault(); // prevent default form submission
+
+    post(route("login"), {
+      onFinish: () => reset("password"),
+    });
+  };
+
+  return (
+    <GuestLayout>
+      <Head title="Log in" />
+
+      {status && (
+        <div className="mb-4 text-sm font-medium text-green-600">{status}</div>
+      )}
+
+      <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-6">
+              <form className="flex flex-col gap-6" onSubmit={submit}>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    onChange={(e) => setData("email", e.target.value)}
+                    value={data.email}
+                    required
+                  />
+                </div>
+                {canResetPassword && (
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                      <a
+                        href="#"
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
+                    <Input
+                      id="password"
+                      value={data.password}
+                      onChange={(e) => setData("password", e.target.value)}
+                      type="password"
+                      required
+                    />
+                  </div>
+                )}
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+              </form>
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </GuestLayout>
+  );
+}
+
+// export const LoginForm = ({
+//   className,
+//   canResetPassword,
+//   status,
+//   ...props
+// }) => {
+//   return (
+//     <div className={cn("flex flex-col gap-6", className)} {...props}>
+//       <Card>
+//         <CardHeader>
+//           <CardTitle className="text-2xl">Login</CardTitle>
+//           <CardDescription>
+//             Enter your email below to login to your account
+//           </CardDescription>
+//         </CardHeader>
+//         <CardContent>
+//         <form onSubmit={submit}>
+//             <div className="flex flex-col gap-6">
+//               <div className="grid gap-2">
+//                 <Label htmlFor="email">Email</Label>
+//                 <Input
+//                   id="email"
+//                   type="email"
+//                   placeholder="m@example.com"
+//                   required
+//                 />
+//               </div>
+//               {canResetPassword && (<div className="grid gap-2">
+//                 <div className="flex items-center">
+//                   <Label htmlFor="password">Password</Label>
+//                   <a
+//                     href="#"
+//                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+//                   >
+//                     Forgot your password?
+//                   </a>
+//                 </div>
+//                 <Input id="password" type="password" required />
+//               </div>)}
+//               <Button type="submit" className="w-full">
+//                 Login
+//               </Button>
+//               <Button variant="outline" className="w-full">
+//                 Login with Google
+//               </Button>
+//             </div>
+//             <div className="mt-4 text-center text-sm">
+//               Don&apos;t have an account?{" "}
+//               <a href="#" className="underline underline-offset-4">
+//                 Sign up
+//               </a>
+//             </div>
+//           </form>
+//         </CardContent>
+//       </Card>
+//     </div>
+//   )
+// }
